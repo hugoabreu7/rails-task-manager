fefeac95e5a852027da3c3d9d1f4824be0171203
Hugo Abreu <hugoabreu90@gmail.com>
2018-11-06 15:25:51 +0900

    Completed all rails CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :tasks_new
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
